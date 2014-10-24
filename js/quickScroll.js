3557edc37e678784fe8463ff77201b4ba3f87b6f
Davison Joseph <davisonjoseph@gmail.com>
2014-10-24 22:51:48 +0530

    scelton plugin

---
+++ js/quickScroll.js
@@ -0,0 +1,46 @@
+(function($){
+$.fn.pageScroll = function() {
+	var pageScrollContainer = '<div class="pageScroll-container" id="PsScroll"><i class="down-scroll"></i><span class="scroll_label">Scroll down</span></div>';
+	var pageScrollDocumentHeight = $(this).css("height");
+        var windowHeight = $(window).height();
+        var initDocHeight = $(document).height();
+        //if(initDocHeight>windowHeight){
+	$("body").append(pageScrollContainer);
+        //}
+	$('#PsScroll').on("click",function(){
+	    var documentHeight = $(document).height()-windowHeight;
+	    var scroll = $(window).scrollTop();
+	    var DocHalfSize = documentHeight/2;
+            if(scroll<=DocHalfSize){
+	    	var scrollAmount = documentHeight;
+	    }else{
+	    	var scrollAmount = 0;
+	    }
+	    $('html,body').animate({ scrollTop: scrollAmount }, 'slow', function () {
+	     	var CurScroll = $(window).scrollTop();
+	    	if(CurScroll==0){
+	    		$(".scroll_label").html("Scroll down");
+	    		$(".pageScroll-container i").removeClass("up-scroll").addClass("down-scroll");
+	    	}else{
+	    		$(".scroll_label").html("Scroll up");
+	    		$(".pageScroll-container i").removeClass("down-scroll").addClass("up-scroll");
+	    	}
+	     });
+	});
+
+	$(window).scroll(function(){
+	    var documentHeight = $(document).height()-windowHeight;;
+	    var scroll = $(window).scrollTop();
+	    var DocHalfSize = documentHeight/2;
+	    if(scroll<=DocHalfSize){
+	    	var scrollAmount = documentHeight;
+	    	$(".scroll_label").html("Scroll down");
+	    	$(".pageScroll-container i").removeClass("up-scroll").addClass("down-scroll");
+	    }else{
+	    	var scrollAmount = 0;
+	    	$(".scroll_label").html("Scroll up");
+	    	$(".pageScroll-container i").removeClass("down-scroll").addClass("up-scroll");
+	    }
+	});
+};
+})(jQuery);
